5a062fa887f6322da0699a55cd100aa45cc820ee
Moamin <moamin.salamah@gmail.com>
2024-10-09 18:27:51 +0200

    categories section

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Main.css";
 import Slider from "./slider/Slider.jsx";
+import Category from "./category/Category.jsx";
 const Main = () => {
   return (
     <div className="main">
       <Slider />
+      <Category />
     </div>
   );
 };
