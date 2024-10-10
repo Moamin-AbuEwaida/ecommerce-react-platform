daad9d847601654e6322216eed11573200a459a7
Moamin <moamin.salamah@gmail.com>
2024-10-10 11:29:08 +0200

    trend section

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,12 +3,14 @@ import "./Main.css";
 import Slider from "./slider/Slider.jsx";
 import Category from "./category/Category.jsx";
 import Collection from "./collection/Collection.jsx";
+import Trend from "./trend/Trend.jsx";
 const Main = () => {
   return (
     <div className="main">
       <Slider />
       <Category />
       <Collection />
+      <Trend />
     </div>
   );
 };
